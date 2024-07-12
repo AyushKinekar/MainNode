05a559251316759b1c73c65da44026c596acddf3
Ayush Kinekar <kinekar.ayush@gmail.com>
2024-07-12 11:34:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let num0=0;
+let num1=1;
+let num2=2;
+let num3=3;
+let num4=4;
+let num5=5;
+let num6=6;
+let num7=7;
+let num8=8;
+let num9=9;
+console.log(num1);
+let number=num2+num3;
+console.log(number)
